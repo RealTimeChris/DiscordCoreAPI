6d97f9b8948a03ee472f7bdceb2c17626b0a967f
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2021-08-11 06:14:33 -0400

    Update Test.hpp

--- Commands/Test.hpp
+++ Commands/Test.hpp
@@ -23,17 +23,8 @@ namespace DiscordCoreAPI {
 
 		virtual  task<void> execute(shared_ptr<BaseFunctionArguments> args) {
 
-			ModifyGuildMemberData dataPackage01;
-			dataPackage01.currentChannelId = "";
-			dataPackage01.deaf = false;
-			dataPackage01.mute = false;
-			dataPackage01.newVoiceChannelId = "";
-			dataPackage01.nick = "TEST NICK";
-			dataPackage01.roleIds = vector<string>{ "" };
-			dataPackage01.guildId = args->eventData.getGuildId();
-			dataPackage01.guildMemberId = args->eventData.getAuthorId();
-
-			GuildMember guildMember01 = args->eventData.discordCoreClient->guildMembers->modifyGuildMemberAsync(dataPackage01).get();
+			
+			vector<Guild> guilds = args->eventData.discordCoreClient->guilds->getAllGuildsAsync().get();
 
 			co_return;
 		}
