f643533325f4424c37a594fc9d04d461d9ab7f89
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2021-12-11 16:02:44 -0500

    Updates!

--- Headers/SSLClient.hpp
+++ Headers/SSLClient.hpp
@@ -6,14 +6,6 @@
 #include "IndexInitial.hpp"
 #include "FoundationEntities.hpp"
 
-#ifndef _WINSOCK_DEPRECATED_NO_WARNINGS
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#endif
-
-#pragma comment(lib, "mbedtls")
-#pragma comment(lib, "mbedx509")
-#pragma comment(lib, "mbedcrypto")
-
 #ifdef _WIN32
 #include <WinSock2.h>
 #include <WS2tcpip.h>
@@ -21,38 +13,41 @@
 #pragma comment(lib,"ws2_32")
 #endif
 
-#include <mbedtls/debug.h>
-#include <mbedtls/platform.h>
-#include <mbedtls/net_sockets.h>
-#include <mbedtls/debug.h>
-#include <mbedtls/ssl.h>
-#include <mbedtls/entropy.h>
-#include <mbedtls/ctr_drbg.h>
-#include <mbedtls/error.h>
-#include <mbedtls/certs.h>
+#pragma comment(lib, "openssl")
+#pragma comment(lib, "libssl")
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 namespace DiscordCoreInternal {
 
+	class SSLContext {
+	public:
+
+		SSL_CTX* context{ nullptr };
+
+		SSL* ssl{ nullptr };
+
+	};
+
 	class DiscordCoreAPI_Dll SSLClient {
 	public:
-        SSLClient(string, string);
+		SSLClient(string, string);
 
-        ~SSLClient();
+		~SSLClient();
 
 	protected:
-        uint32_t returnCode{ 0 };
-        uint32_t exitCode = MBEDTLS_EXIT_FAILURE;
-        mbedtls_net_context serverFd{};
-        uint32_t flags{ 0 };
-        string path{ "/?v=9&encoding=etf" };
-        unsigned char buffer[1024];
-        const char* pers = "ssl_client1";
-
-        mbedtls_entropy_context entropy{};
-        mbedtls_ctr_drbg_context ctrDebug{};
-        mbedtls_ssl_context ssl{};
-        mbedtls_ssl_config config{};
-        mbedtls_x509_crt cacert{};
+		uint32_t sfd{ static_cast<uint32_t>(~0) };
+		vector<uint8_t> outputBuffer{};
+		vector<uint8_t> inputBuffer{};
+		SSLContext* ssl{ nullptr };
+		bool nonblocking{ false };
+		uint64_t bytesOut{ 0 };
+		uint64_t lastTime{ 0 };
+		uint64_t bytesIn{ 0 };
+		string hostname{ "" };
+		string cipher{ "" };
+		string port{ "" };
 	};
 
 }
