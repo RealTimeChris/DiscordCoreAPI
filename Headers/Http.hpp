24fb53fe272add67d1dd9a9ba3db7c8d7fb6c5f9
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-01-02 23:26:30 -0500

    Updates!

--- Headers/Http.hpp
+++ Headers/Http.hpp
@@ -15,6 +15,113 @@ namespace DiscordCoreInternal {
 	const string youtubeCertPath{ "C:/Program Files/Common Files/SSL/certs/gtsr1.pem" };
 	const string googleCertPath{ "C:/Program Files/Common Files/SSL/certs/gtsr1.pem" };
 
+	class HttpHeader {
+	public:
+
+		HttpHeader(nullptr_t) {};
+
+		HttpHeader(string key, string value) {
+			this->value = value;
+			this->key = key;
+		}
+
+		virtual void constructValue(shared_ptr<RateLimitData> pRateLimitData, map<string, shared_ptr<HttpHeader>> headers) {};
+
+		virtual ~HttpHeader() {};
+
+		string value{ "" };
+		string key{ "" };
+	};
+
+	class RateLimitRetryAfter : public HttpHeader {
+	public:
+
+		RateLimitRetryAfter(string key, string value) : HttpHeader(key, value) {};
+
+		virtual void constructValue(shared_ptr<RateLimitData> pRateLimitData, map<string, shared_ptr<HttpHeader>> headers) {
+			if (headers.contains("x-ratelimit-retry-after")) {
+				pRateLimitData->msRemain = static_cast<int64_t>(static_cast<float>(stoll(headers.at("x-ratelimit-retry-after")->value)) * 1000.0f);
+			}
+		}
+	};
+
+	class RateLimitMsRemain : public HttpHeader {
+	public:
+
+		RateLimitMsRemain(string key, string value) : HttpHeader(key, value) {};
+
+		virtual void constructValue(shared_ptr<RateLimitData> pRateLimitData, map<string, shared_ptr<HttpHeader>> headers) {
+			if (headers.contains("x-ratelimit-remaining")) {
+				pRateLimitData->getsRemaining = stol(headers.at("x-ratelimit-remaining")->value);
+			}
+		}
+
+	};
+
+	class RateLimitLimit : public HttpHeader {
+	public:
+
+		RateLimitLimit(string key, string value) : HttpHeader(key, value) {};
+
+		virtual void constructValue(shared_ptr<RateLimitData> pRateLimitData, map<string, shared_ptr<HttpHeader>> headers) {
+			if (headers.contains("x-ratelimit-limit")) {
+				pRateLimitData->totalGets = stol(headers.at("x-ratelimit-limit")->value.c_str());
+			}
+		}
+
+	};
+
+	class RateLimitResetAfter : public HttpHeader {
+	public:
+
+		RateLimitResetAfter(string key, string value) : HttpHeader(key, value) {};
+
+		virtual void constructValue(shared_ptr<RateLimitData> pRateLimitData, map<string, shared_ptr<HttpHeader>> headers) {
+			if (headers.contains("x-ratelimit-reset-after")) {
+				pRateLimitData->msRemain = static_cast<int64_t>(stod(headers.at("x-ratelimit-reset-after")->value) * 1000.0f);
+			}
+		}
+
+	};
+
+	class RateLimitBucket : public HttpHeader {
+	public:
+
+		RateLimitBucket(string key, string value) : HttpHeader(key, value) {};
+
+		virtual void constructValue(shared_ptr<RateLimitData> pRateLimitData, map<string, shared_ptr<HttpHeader>> headers) {
+			if (headers.contains("x-ratelimit-bucket")) {
+				pRateLimitData->bucket = headers.at("x-ratelimit-bucket")->value;
+			}
+		}
+
+	};
+
+	class HttpHeaderBuilder {
+	public:
+
+		static shared_ptr<HttpHeader> getHeader(string key, string value) {
+			if (key.contains("x-ratelimit-remaining")) {
+				return make_shared<RateLimitMsRemain>(key, value);
+			}
+			else if (key.contains("x-ratelimit-reset-after")) {
+				return make_shared<RateLimitResetAfter>(key, value);
+			}
+			else if (key.contains("x-ratelimit-bucket")) {
+				return make_shared<RateLimitBucket>(key, value);
+			}
+			else if (key.contains("x-ratelimit-limit")) {
+				return make_shared<RateLimitLimit>(key, value);
+			}
+			else if (key.contains("x-ratelimi-retry-after")) {
+				return make_shared<RateLimitRetryAfter>(key, value);
+			}
+			else {
+				return make_shared<HttpHeader>(key, value);
+			}
+		};
+	};
+
 	class HttpRnRBuilder;
 
 	class DiscordCoreAPI_Dll HttpConnection {
@@ -51,13 +158,13 @@ namespace DiscordCoreInternal {
 
 		HttpData handleHeaders(HttpWorkloadData& workloadData, shared_ptr<RateLimitData> pRateLimitData);
 
-		map<string, string> getResponseHeaders();
+		map<string, shared_ptr<HttpHeader>> getResponseHeaders();
 
 	protected:
 
+		map<string, shared_ptr<HttpHeader>> headers{};
 		bool doWeHaveContentSize{ false };
 		bool doWeHaveHeaders{ false };
-		map<string, string> headers{};
 		int64_t contentSize{ -1 };
 		bool isItChunked{ false };
 		string rawInput{ "" };
@@ -181,7 +288,7 @@ namespace DiscordCoreInternal {
 		static HttpData executeByRateLimitData(HttpWorkloadData workload, shared_ptr<RateLimitData> rateLimitDataNew, bool printResult);
 
 		static HttpData HttpRequest(HttpWorkloadData&, shared_ptr<RateLimitData>);
-		
+
 
 	};
 }
