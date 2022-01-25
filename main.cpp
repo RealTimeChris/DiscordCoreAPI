bdf77accfa9ad7f3dc8d9da99964bc19adccba37
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-01-25 06:38:55 -0500

    Updates!

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
 // main.cpp - Main entry point.
+// Jun 17, 2021
+// Chris M.
 // https://github.com/RealTimeChris
 
-#include "Index.hpp"
-#include "Commands/CommandList.hpp"
+#include "Headers/Index.hpp"
 
-void onBoot01() {
-    std::vector<DiscordCoreAPI::ActivityData> activities;
-    DiscordCoreAPI::ActivityData activity;
+void onBoot01(DiscordCoreAPI::DiscordCoreClient* thePtr) {
+    auto botUser = thePtr->getBotUser();
+}
+
+void onBoot02(DiscordCoreAPI::DiscordCoreClient* thePtr) {
+    std::vector<DiscordCoreAPI::ActivityData> activities{};
+    DiscordCoreAPI::ActivityData activity{};
     activity.name = "!help for my commands!";
     activity.type = DiscordCoreAPI::ActivityType::Game;
     activities.push_back(activity);
-    DiscordCoreAPI::UpdatePresenceData updateData{};
-    updateData.activities = activities;
-    updateData.afk = false;
-    updateData.status = "online";
-    DiscordCoreAPI::getBotUser().updatePresence(updateData);
+    thePtr->getBotUser().updatePresence({ .activities = activities, .status = "online",.afk = false });
 }
 
-int main()
+int32_t main()
 {
-    winrt::init_apartment();
     std::string botToken = "YOUR_BOT_TOKEN_HERE";
     std::vector<DiscordCoreAPI::RepeatedFunctionData> functionVector;
     DiscordCoreAPI::RepeatedFunctionData function01;
@@ -27,16 +27,6 @@ int main()
     function01.intervalInMs = 500;
     function01.repeated = false;
     functionVector.push_back(function01);
-    DiscordCoreAPI::CacheOptions cacheOptions{};
-    cacheOptions.cacheChannels = true;
-    cacheOptions.cacheGuildMembers = true;
-    cacheOptions.cacheGuilds = true;
-    cacheOptions.cacheRoles = true;
-    cacheOptions.cacheUsers = true;
-    DiscordCoreAPI::DiscordCoreClient::setup(botToken, "!", functionVector, cacheOptions);
-    DiscordCoreAPI::CommandController::registerFunction(std::vector<std::string>{"test"}, std::move(std::make_unique<DiscordCoreAPI::Test>()));
-
-    DiscordCoreAPI::DiscordCoreClient::thisPointer->runBot();
+    auto thePtr = std::make_unique<DiscordCoreAPI::DiscordCoreClient>(botToken, "!", functionVector, DiscordCoreAPI::CacheOptions{ .cacheGuildMembers = true, .cacheChannels = true, .cacheGuilds = true, .cacheRoles = true, .cacheUsers = true });
     return 0;
-};
-
+}
