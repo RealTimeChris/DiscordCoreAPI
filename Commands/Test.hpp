fd28168fd314b972995b4ca380269954624841b4
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-02-06 06:00:36 -0500

    Update Test.hpp

--- Commands/Test.hpp
+++ Commands/Test.hpp
@@ -4,7 +4,7 @@
 #ifndef TEST
 #define TEST
 
-#include "../Input Files/Include/Index.hpp"
+#include "../../Include/Index.hpp"
 
 namespace DiscordCoreAPI {
 
@@ -26,7 +26,31 @@ namespace DiscordCoreAPI {
 		}
 
 		virtual void execute(std::unique_ptr<BaseFunctionArguments> args) {
-
+			EmbedData msgEmbed{};
+			GetGuildMemberData getData{};
+			getData.guildId = args->eventData.getGuildId();
+			getData.guildMemberId = args->eventData.getAuthorId();
+			GuildMember guildMember = GuildMembers::getGuildMemberAsync(getData).get();
+			msgEmbed.setAuthor(guildMember.nick, guildMember.user.avatar);
+			msgEmbed.setColor("FeFeFe");
+			if (args->argumentsArray.size() > 0) {
+				msgEmbed.setDescription(args->argumentsArray[0]);
+			}
+			else {
+				msgEmbed.setDescription("THIS IS A TEST MESSAGE!");
+			}
+			msgEmbed.setTimeStamp(getTimeAndDate());
+			msgEmbed.setTitle("__**Test Embed**__");
+			RespondToInputEventData dataResponse{ args->eventData };
+			if (args->eventData.eventType == InputEventType::Regular_Message) {
+				dataResponse.type = InputEventResponseType::Regular_Messag;
+			}
+			else {
+				dataResponse.type = InputEventResponseType::Interaction_Response;
+			}
+			dataResponse.addMessageEmbed(msgEmbed);
+			InputEvents::deleteInputEventResponseAsync(args->eventData);
+			InputEvents::respondToEvent(dataResponse);
 			return;
 		}
 		virtual ~Test() {};
