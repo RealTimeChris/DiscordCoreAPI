82e94450a13e94338724e891a5f70934e23d4b4c
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-02-09 08:28:09 -0500

    Update DllMain.cpp

--- Source/DllMain.cpp
+++ Source/DllMain.cpp
@@ -1,4 +1,26 @@
 // dllmain.cpp : Defines the entry point for the DLL application..
 #include "Index.hpp"
 
+#ifdef _WIN32
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,
+    DWORD fdwReason,
+    LPVOID lpReserved) {
 
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        break;
+
+    case DLL_THREAD_ATTACH:
+        break;
+
+    case DLL_THREAD_DETACH:
+        break;
+
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+#endif
