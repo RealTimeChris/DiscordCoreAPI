49e85a083e0e87472e8e04b705c9ea6af9b53b80
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-02-10 00:22:31 -0500

    Update YouTubeAPI.cpp

--- Source/YouTubeAPI.cpp
+++ Source/YouTubeAPI.cpp
@@ -262,10 +262,17 @@ namespace DiscordCoreAPI {
 		std::string sliceStr = ":function\\(a,b\\)\\{return a\\.slice\\(b\\)\\}";
 		std::string spliceStr = ":function\\(a,b\\)\\{a\\.splice\\(0,b\\)\\}";
 		std::string swapStr = ":function\\(a,b\\)\\{var c=a\\[0\\];a\\[0\\]=a\\[b(?:%a\\.length)?\\];a\\[b(?:%a\\.length)?\\]=c(?:;return a)?\\}";
+#ifdef _WIN32
 		std::regex reverseRegexp("(?:^|,)(" + jsKeyStr + ")" + reverseStr);
 		std::regex sliceRegexp("(?:^|,)(" + jsKeyStr + ")" + sliceStr);
 		std::regex spliceRegexp("(?:^|,)(" + jsKeyStr + ")" + spliceStr);
 		std::regex swapRegexp("(?:^|,)(" + jsKeyStr + ")" + swapStr);
+#else
+		std::regex reverseRegexp(jsVarStr + reverseStr);
+		std::regex sliceRegexp(jsVarStr + sliceStr);
+		std::regex spliceRegexp(jsVarStr + spliceStr);
+		std::regex swapRegexp(jsVarStr + swapStr);
+#endif
 		std::string newString = html5PlayerPageBody;
 		std::vector<std::string> tokenActions;
 		std::regex actionsObjRegexp("var (" + jsVarStr + ")=\\{((?:(?:" + jsKeyStr + reverseStr + "|" + jsKeyStr + sliceStr + "|" + jsKeyStr + spliceStr + "|" + jsKeyStr + swapStr + "),?\\r?\\n?)+)\\};", std::regex_constants::ECMAScript);
