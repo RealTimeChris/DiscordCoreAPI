081f1c30d40ee13e162434454f2cab89912e566b
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-02-12 07:12:22 -0500

    Revert "Update CoRoutine.hpp"
    This reverts commit 1820f965962b31ab2c779dccf032b1f027b92193.

--- Include/CoRoutine.hpp
+++ Include/CoRoutine.hpp
@@ -114,9 +114,7 @@ namespace DiscordCoreAPI {
         CoRoutine(std::coroutine_handle<CoRoutine<ReturnType>::promise_type> coroutineHandleNew) : coroutineHandle(coroutineHandleNew) {};
 
         ~CoRoutine() {
-            if (this->coroutineHandle) {
-                this->cancel();
-                while (!this->coroutineHandle.done()) {};
+            if (this->coroutineHandle && this->coroutineHandle.done()) {
                 this->coroutineHandle.destroy();
             }
         }
@@ -276,9 +274,7 @@ namespace DiscordCoreAPI {
         CoRoutine(std::coroutine_handle<CoRoutine<void>::promise_type> coroutineHandleNew) : coroutineHandle(coroutineHandleNew) {};
 
         ~CoRoutine() {
-            if (this->coroutineHandle) {
-                this->cancel();
-                while (!this->coroutineHandle.done()) {};
+            if (this->coroutineHandle && this->coroutineHandle.done()) {
                 this->coroutineHandle.destroy();
             }
         }
