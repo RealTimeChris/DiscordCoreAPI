2c67f85d6cc9d8ec55c592e5bf3d19983c805db4
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-02-15 05:10:59 -0500

    Updates!

--- Source/VoiceConnection.cpp
+++ Source/VoiceConnection.cpp
@@ -166,11 +166,9 @@ namespace DiscordCoreAPI {
 			this->voiceSocketAgent = std::make_unique<DiscordCoreInternal::VoiceSocketAgent>(this->voiceConnectInitData, this->baseSocketAgent);
 			this->doWeReconnect = &this->voiceSocketAgent->doWeReconnect;
 			this->voiceConnectionData = &this->voiceSocketAgent->voiceConnectionData;
-			if (this->theTask != nullptr && this->theTask->getStatus() == CoRoutineStatus::Running){
-				this->theTask->cancel();
-				this->theTask.reset(nullptr);
+			if (this->theTask == nullptr) {
+				this->theTask = std::make_unique<CoRoutine<void>>(this->run());
 			}
-			this->theTask = std::make_unique<CoRoutine<void>>(this->run());
 		}
 		catch (...) {
 			DiscordCoreAPI::reportException("VoiceConnection::connect()");
