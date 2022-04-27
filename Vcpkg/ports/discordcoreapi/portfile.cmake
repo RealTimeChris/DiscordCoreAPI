dcf49a6900241ebc0ad19fca022d5a009c22e141
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-04-26 20:15:06 -0400

    Updates!

--- Vcpkg/ports/discordcoreapi/portfile.cmake
+++ Vcpkg/ports/discordcoreapi/portfile.cmake
@@ -1,11 +1,11 @@
 if(VCPKG_TARGET_IS_LINUX)
-    message(WARNING "Discordcoreapi only supports g++ 11 on linux.")
+    message(WARNING "DiscordCoreAPI only supports g++ 11 on linux.")
 endif()
 
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
-	REF 3cfa8123167ba2e37f03c2046659e8be149846a7
+	REF fdd88bb3b659ca1364b2e42029e3a6eb44bfa348
 	SHA512 c2339d5f25727200cd6276621b105f544bf5e25ff615ad14549d699b6249889c8f8b41361d25de76e18f4863d8894dcff0fd239d07d81af032a9c8db602b43ff
 	HEAD_REF main
 )
