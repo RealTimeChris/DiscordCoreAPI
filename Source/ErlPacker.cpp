c3acf28340ea2d28a283d8d3fd61d0f0abeec3aa
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-04-30 19:55:50 -0400

    Updates!

--- Source/ErlPacker.cpp
+++ Source/ErlPacker.cpp
@@ -252,7 +252,8 @@ namespace DiscordCoreInternal {
 		}
 		ReturnType newValue{ 0 };
 		for (uint32_t x = 0; x < sizeof(ReturnType); x += 1) {
-			newValue |= static_cast<ReturnType>(buffer.buffer.data()[static_cast<uint64_t>(buffer.offSet + x)] << (x * byteSize));
+			newValue |= static_cast<ReturnType>(buffer.buffer.data()[static_cast<uint64_t>(buffer.offSet) + static_cast<uint64_t>(x)]
+				<< (static_cast<uint64_t>(x) * static_cast<uint64_t>(byteSize)));
 		}
 		buffer.offSet += sizeof(ReturnType);
 		ErlPacker::etfByteOrder(newValue, theValue);
