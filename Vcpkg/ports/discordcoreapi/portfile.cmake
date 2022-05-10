fa7bf1b333817860616499e13394a8b7ea82b97a
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-05-10 13:20:53 -0400

    Update portfile.cmake

--- Vcpkg/ports/discordcoreapi/portfile.cmake
+++ Vcpkg/ports/discordcoreapi/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+    #file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
 file(
