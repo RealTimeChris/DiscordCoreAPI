1bc5f63aa7242e1e2ac0bcfdcd537fac3fc92cdf
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-05-19 22:37:18 -0400

    Update Http.cpp

--- Source/Http.cpp
+++ Source/Http.cpp
@@ -353,7 +353,8 @@ namespace DiscordCoreInternal {
 				}
 				returnData = this->executeByRateLimitData(workload, theConnection);
 			} else if (this->doWePrintHttpError) {
-				std::cout << DiscordCoreAPI::shiftToBrightRed() << "Http Error: Code = " << std::to_string(returnData.responseCode) + ", Message = " + returnData.responseMessage
+				std::cout << DiscordCoreAPI::shiftToBrightRed()
+						  << workload.callStack + " Error : Code = " << std::to_string(returnData.responseCode) + ", Message = " + returnData.responseMessage
 						  << DiscordCoreAPI::reset() << std::endl;
 			}
 		}
