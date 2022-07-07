4aea263956268c9091245c58432941242609a32e
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-07 02:58:25 -0400

    Update portfile.cmake

--- Vcpkg/ports/discordcoreapi/portfile.cmake
+++ Vcpkg/ports/discordcoreapi/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
 	REF 58c8897721541aa0090268d7baaff41ac4c023ed
-	SHA512 26b577ae82a4733741917cf91cbed61a892a6b7be90f54a3b743ab2499cd3d0dec74380df613f0902285d02b1bd969fbf9cb661614e654e66dfee22104c5cf10
+	SHA512 
 	HEAD_REF main
 )
 
@@ -17,6 +17,13 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 
 file(
 	INSTALL "${SOURCE_PATH}/License.md"
