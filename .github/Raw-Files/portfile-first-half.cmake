406559bde7b6d8a6b43bf431193f198f68d78e5a
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-19 15:17:26 -0400

    Revert "Merge branch 'main' of https://github.com/RealTimeChris/DiscordCoreAPI"
    This reverts commit abc4de6c8a1f6125ab241f873a7a666e63ef6c4e, reversing
    changes made to ca6b6e444a106ad37530cbb0c2999e0a9117ed71.

--- .github/Raw-Files/portfile-first-half.cmake
+++ .github/Raw-Files/portfile-first-half.cmake
@@ -5,5 +5,4 @@ endif()
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
-	REF 34a9544df26e01b17254f90d1b939a24ccf579ec
-74bac5adf589e241a9e1bf748aa909f363ff3820
+	REF 
