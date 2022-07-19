bce15dcd1616cd5cf37d9d7caaf007f5ce8aea3d
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-19 19:19:48 -0400

    Updates!

--- CMake/Raw-Files/Portfile-The-First.cmake
+++
@@ -1,8 +0,0 @@
-if(VCPKG_TARGET_IS_LINUX)
-    message(WARNING "DiscordCoreAPI only supports g++ 11 on linux.")
-endif()
-
-vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO RealTimeChris/DiscordCoreAPI
-	REF 
