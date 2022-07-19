4847d9cf95177fdc11ccced65d14bc19cfbdb39f
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-19 13:36:03 -0400

    Revert "Updates!"
    This reverts commit 1857a1c3226a16822fd6bba221002836685c1a3f.

--- Vcpkg/ports/discordcoreapi/portfile.cmake
+++ Vcpkg/ports/discordcoreapi/portfile.cmake
@@ -6,4 +6,25 @@ vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
 	REF fababbe98a5b0158b8168b1657cdaed28bffc374
-	SHA512 c43df10ea0d0102a60a8a033cb079c840dc49523f1cda1d18c26c3c224da48a26dd37a298e0688cd7fa66337b86121c99f30add44dc900f9e0cacf9d78c7a929
+	SHA512 c43df10ea0d0102a60a8a033cb079c840dc49523f1cda1d18c26c3c224da48a26dd37a298e0688cd7fa66337b86121c99f30add44dc900f9e0cacf9d78c7a929	HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+file(
+	INSTALL "${SOURCE_PATH}/License.md"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright
+)
