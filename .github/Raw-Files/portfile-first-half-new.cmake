944af3854147ee81127869d30d9557aa791ed9aa
realtimechris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-19 16:51:18 +0000

    Updates!

--- .github/Raw-Files/portfile-first-half-new.cmake
+++ .github/Raw-Files/portfile-first-half-new.cmake
@@ -6,8 +6,7 @@ vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
 	REF fababbe98a5b0158b8168b1657cdaed28bffc374
-	SHA512 0
-	HEAD_REF main
+	SHA512 c43df10ea0d0102a60a8a033cb079c840dc49523f1cda1d18c26c3c224da48a26dd37a298e0688cd7fa66337b86121c99f30add44dc900f9e0cacf9d78c7a929	HEAD_REF main
 )
 
 vcpkg_cmake_configure(
