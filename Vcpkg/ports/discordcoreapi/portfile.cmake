e0f992fa504ebf38d330b9343e981b36dbf0ed39
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-19 16:48:39 -0400

    Updates!

--- Vcpkg/ports/discordcoreapi/portfile.cmake
+++ Vcpkg/ports/discordcoreapi/portfile.cmake
@@ -6,9 +6,8 @@ vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
 	REF a5291396d7f2773272f640178fd0d02db34d3c29
-	SHA512 
-        SHA512
-51e901fd774bf250c99dc0d627f7c066d81b3c64499648a11ae40c5ee000c4cfe80dad3fcbcc3e6aaedc2f582bceefbbc1d60b10bb5278a0f1a28c35fbb952d7	HEAD_REF main
+	SHA512 0
+	HEAD_REF main
 )
 
 vcpkg_cmake_configure(
