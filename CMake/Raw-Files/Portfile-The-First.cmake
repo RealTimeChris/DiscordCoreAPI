9aeaf1ee330c54bb9aa386a6a476f0d0ed56fd37
realtimechris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-20 02:55:51 +0000

    Updates! [skip ci]

--- CMake/Raw-Files/Portfile-The-First.cmake
+++ CMake/Raw-Files/Portfile-The-First.cmake
@@ -5,7 +5,7 @@ endif()
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
-	REF 6171a87bfc77dd308428072f92129ca7b68ead1f
+	REF 1f7e65d583adf8c34358c073bfd89ec379cf3254
 	SHA512 0
 	HEAD_REF main
 )
