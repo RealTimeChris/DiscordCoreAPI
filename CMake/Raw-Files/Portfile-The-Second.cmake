65437b2f5eeef8d365dafb5c9ec9a508cc71f5d9
realtimechris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-20 00:11:21 +0000

    Updates!

---
+++ CMake/Raw-Files/Portfile-The-Second.cmake
@@ -0,0 +1,31 @@
+if(VCPKG_TARGET_IS_LINUX)
+    message(WARNING "DiscordCoreAPI only supports g++ 11 on linux.")
+endif()
+
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO RealTimeChris/DiscordCoreAPI
+	REF de8480001168089127ff60768f775e48855219dc
+	SHA512 4384c41e85cf9041743da6499113f494b9ef74e6d916d841c44a50d773d93199a5699fe04ab943b30a9106b1d3bdf1f530521529b0a62e611e4f13aa800d93c8
+	HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+file(
+	INSTALL "${SOURCE_PATH}/License.md"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright
+)
