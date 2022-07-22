91a7bb1b54282178b4f97d78cf1295f5447d4309
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-21 22:16:44 -0400

    Update SSLClients.hpp

--- Include/discordcoreapi/SSLClients.hpp
+++ Include/discordcoreapi/SSLClients.hpp
@@ -172,17 +172,7 @@ namespace DiscordCoreInternal {
 		SSLWrapper ssl{};
 	};
 
-	enum class ProcessIOResult : int8_t {
-		No_Error = 0,
-		Disconnected = 1,
-		SSL_Error = 2,
-		SSL_Want_Read = 3,
-		SSL_Want_Write = 4,
-		SSL_Zero_Return = 5,
-		Select_Failure = 6,
-		Select_No_Return = 7,
-		Nothing_To_Write = 8
-	};
+	enum class ProcessIOResult : int8_t { No_Error = 0, Disconnected = 1, SSL_Error = 2, SSL_Zero_Return = 3, Select_Failure = 4, Select_No_Return = 5, Nothing_To_Write = 6 };
 
 	class DiscordCoreAPI_Dll SSLDataInterface {
 	  public:
