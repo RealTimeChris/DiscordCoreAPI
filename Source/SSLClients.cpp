e25b55946cdc34aad125d494bbba7d1e0d954b19
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-08-04 13:44:21 -0700

    Updates! [skip ci]

--- Source/SSLClients.cpp
+++ Source/SSLClients.cpp
@@ -488,7 +488,7 @@ namespace DiscordCoreInternal {
 			this->theStreamAddress.sin_port = DiscordCoreAPI::reverseByteOrder(static_cast<unsigned short>(stoi(portNew)));
 			this->theStreamAddress.sin_family = AF_INET;
 		} else {
-			this->theStreamAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+			this->theStreamAddress.sin_addr.s_addr = inet_addr(baseUrlNew.c_str());
 			this->theStreamAddress.sin_port = DiscordCoreAPI::reverseByteOrder(static_cast<unsigned short>(stoi(portNew)));
 			this->theStreamAddress.sin_family = AF_INET;
 		}
