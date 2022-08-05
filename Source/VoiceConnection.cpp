9af5e5b555827624cf6075cea50ca12571204e3c
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-08-05 06:15:16 -0400

    Updates! [skip ci]

--- Source/VoiceConnection.cpp
+++ Source/VoiceConnection.cpp
@@ -79,8 +79,7 @@ namespace DiscordCoreAPI {
 	}
 
 	AreWeInTimeResult VoicePayload::areWeInTime(int64_t originalGlobalTimeStampInMs, int64_t currentTimeStampInMs, uint32_t originalTimeStamp, uint32_t currentTimeStamp) {
-		if ((currentTimeStampInMs - originalGlobalTimeStampInMs / 20) == (currentTimeStamp - originalTimeStamp / 960) ||
-			(currentTimeStampInMs - originalGlobalTimeStampInMs / 20) == (currentTimeStamp - originalTimeStamp / 960) + 1) {
+		if ((currentTimeStampInMs - originalGlobalTimeStampInMs / 20) <= (currentTimeStamp - originalTimeStamp / 960)) {
 			return AreWeInTimeResult::Now;
 		} else if (((currentTimeStampInMs - originalGlobalTimeStampInMs) / 20) > ((currentTimeStamp - originalTimeStamp) / 960)) {
 			std::cout << "VALUE 01: " << ((currentTimeStampInMs - originalGlobalTimeStampInMs) / 20) << ", VALUE 02: " << ((currentTimeStamp - originalTimeStamp) / 960)
