7c2951eb7e4529c0a5f1b1d4477b644d3195c270
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-08-07 07:11:48 -0400

    Updates! [skip ci]

--- Source/VoiceConnection.cpp
+++ Source/VoiceConnection.cpp
@@ -1024,7 +1024,7 @@ namespace DiscordCoreAPI {
 			}
 			if (theUpsampledVector.size() > 0) {
 				for (int32_t x = 0; x < theUpsampledVector.size(); x++) {
-					if (theUpsampledVector[x] >= INT16_MAX || theUpsampledVector[x] <= INT16_MIN) {
+					if (theUpsampledVector[x] >= INT16_MAX || theUpsampledVector[x] <= INT16_MIN || theUpsampledVector[x] >= UINT16_MAX) {
 						opus_int16 theInt = theUpsampledVector[x] / voiceUserCount;
 						theDownsampledVector[x] = theInt;
 					} else {
