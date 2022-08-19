ef0e361415c6f4d92a2337630164d2f4f658935a
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-08-18 23:48:44 -0400

    Updates!

--- Source/AudioEncoder.cpp
+++ Source/AudioEncoder.cpp
@@ -54,7 +54,7 @@ namespace DiscordCoreAPI {
 		this->encoder = opus_encoder_create(this->sampleRate, this->nChannels, OPUS_APPLICATION_AUDIO, &error);
 		auto theResult = opus_encoder_ctl(this->encoder, OPUS_SET_SIGNAL(OPUS_SIGNAL_MUSIC));
 		if (theResult != OPUS_OK) {
-			std::cout << "Failed to set the Opus signal type." << std::endl;
+			throw std::runtime_error{ "Failed to set the Opus signal type." };
 		}
 	}
 
