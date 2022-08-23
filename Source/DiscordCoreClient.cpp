453576a871c8b7245d49fb49f04d1f7bc24951aa
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-08-23 05:34:37 -0400

    Updates!

--- Source/DiscordCoreClient.cpp
+++ Source/DiscordCoreClient.cpp
@@ -203,10 +203,8 @@ namespace DiscordCoreAPI {
 		std::vector<uint32_t> theVector{};
 		uint32_t theWorkerCount = shardCount <= threadCount ? shardCount : threadCount;
 		uint32_t extraShards = static_cast<uint32_t>(shardCount % theWorkerCount);
-		std::cout << "EXTRA SHARD COUNT: " << extraShards << std::endl;
 		for (uint32_t x = 0; x < theWorkerCount; ++x) {
 			theVector.push_back(static_cast<uint32_t>(floor((static_cast<float>(shardCount) / static_cast<float>(threadCount)))));
-			std::cout << "THE COUNT: " << static_cast<uint32_t>(floor((static_cast<float>(shardCount) / static_cast<float>(threadCount)))) << std::endl;
 		}
 		uint32_t theIndex{};
 		while (extraShards > 0) {
@@ -239,7 +237,6 @@ namespace DiscordCoreAPI {
 		}
 		std::vector<uint32_t> theDimensions = collectWorkerDimensions(this->configManager.getTotalShardCount(), std::thread::hardware_concurrency());
 		uint32_t baseSocketCount = theDimensions.size();
-		std::cout << "THE BASE SOCKET COUNT: " << baseSocketCount << std::endl;
 		if (this->configManager.getConnectionAddress() == "") {
 			this->configManager.setConnectionAddress(gatewayData.url.substr(gatewayData.url.find("wss://") + std::string("wss://").size()));
 		}
