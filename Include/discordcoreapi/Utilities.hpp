82a829997ed0a20493156ec9295f1bfb9c5c0548
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-08-24 23:22:55 -0400

    Updates!

--- Include/discordcoreapi/Utilities.hpp
+++ Include/discordcoreapi/Utilities.hpp
@@ -479,7 +479,7 @@ namespace DiscordCoreAPI {
 		return lhs;
 	}
 
-	inline std::basic_string<char> operator+(std::basic_string<char, std::char_traits<char>, std::allocator<char>> lhs, StringWrapper rhs) {
+	inline std::basic_string<char> operator+(const std::basic_string<char, std::char_traits<char>, std::allocator<char>>& lhs, StringWrapper rhs) {
 		std::stringstream theStream{};
 		theStream << lhs << rhs;
 		return theStream.str();
