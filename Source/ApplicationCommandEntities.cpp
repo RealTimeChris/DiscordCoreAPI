241c037c5a900d0132b167c5bc73bef059b1393a
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-08-30 20:41:03 -0400

    Updates! (Updated the license)

--- Source/ApplicationCommandEntities.cpp
+++ Source/ApplicationCommandEntities.cpp
@@ -1,17 +1,20 @@
-/*
-	DiscordCoreAPI, A bot library for Discord, written in C++, and featuring explicit multithreading through the usage of custom, asynchronous C++ CoRoutines.
+<one line to give the library's name and a brief idea of what it does.>
+Copyright (C) <year>  <name of author>
 
-	Copyright 2021, 2022 Chris M. (RealTimeChris)
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
 
-	This file is part of DiscordCoreAPI.
-	DiscordCoreAPI is free software: you can redistribute it and/or modify it under the terms of the GNU
-	General Public License as published by the Free Software Foundation, either version 3 of the License,
-	or (at your option) any later version.
-	DiscordCoreAPI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-	even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-	You should have received a copy of the GNU General Public License along with DiscordCoreAPI.
-	If not, see <https://www.gnu.org/licenses/>.
-*/
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+USA
 /// ApplicationCommandEntities.cpp - Source file for the ApplicationCommand classes and structs.
 /// Aug 25, 2021
 /// https://discordcoreapi.com
