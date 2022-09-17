0021f68dfec59d5796239c95709167fa0b17c118
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-09-17 13:20:09 -0400

    Update ErlPacker.cpp

--- Source/ErlPacker.cpp
+++ Source/ErlPacker.cpp
@@ -66,18 +66,18 @@ namespace DiscordCoreInternal {
 			this->appendNewFloatExt(theFloat);
 		} else if (jsonData.get_number_type() == simdjson::ondemand::number_type::unsigned_integer) {
 			auto theInt = jsonData.get_uint64().take_value();
-			if (theInt <= 255) {
+			if (theInt <= 255 && theInt >= 0) {
 				this->appendSmallIntegerExt(static_cast<uint8_t>(theInt));
-			} else if (theInt <= std::numeric_limits<uint32_t>::max()) {
+			} else if (theInt <= std::numeric_limits<uint32_t>::max() && theInt >= 0) {
 				this->appendIntegerExt(static_cast<uint32_t>(theInt));
 			} else {
 				this->appendUnsignedLongLong(theInt);
 			}
 		} else if (jsonData.get_number_type() == simdjson::ondemand::number_type::signed_integer) {
 			auto theInt = jsonData.get_int64().take_value();
-			if (theInt <= 127) {
+			if (theInt <= 127 && theInt >= -127) {
 				this->appendSmallIntegerExt(static_cast<uint8_t>(theInt));
-			} else if (theInt <= std::numeric_limits<int32_t>::max()) {
+			} else if (theInt <= std::numeric_limits<int32_t>::max() && theInt >= std::numeric_limits<int32_t>::min()) {
 				this->appendIntegerExt(static_cast<uint32_t>(theInt));
 			} else {
 				this->appendUnsignedLongLong(static_cast<uint64_t>(theInt));
