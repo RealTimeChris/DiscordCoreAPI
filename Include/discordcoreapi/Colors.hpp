6041d793d7a8bdb8ee655603335ca8237d471fe3
DJ::Ötzi <58144390+DJOetzi@users.noreply.github.com>
2022-11-03 08:09:16 +0100

    add colors
    still needs testing!

--- Include/discordcoreapi/Colors.hpp
+++ Include/discordcoreapi/Colors.hpp
@@ -19,3 +19,59 @@
 /// https://discordcoreapi.com
 /// \file Colors.hpp
 #pragma once
+#ifndef COLORS
+
+#include <string>
+
+namespace DiscordCoreAPI {
+	/// Several premade color constants ready to use for embeds
+	namespace Colors {
+		const std::string
+			white = "FFFFFF",
+			discord_white = "FFFFFE",
+			light_gray = "C0C0C0",
+			gray = "808080",
+			dark_gray = "404040",
+			black = "000000",
+			discord_black = "000001",
+			red = "FF0000",
+			pink = "FFAFAF",
+			orange = "FFC800",
+			yellow = "FFFF00",
+			green = "00FF00",
+			magenta = "FF00FF",
+			cyan = "00FFFF",
+			blue = "0000FF",
+			light_sea_green = "1ABC9C",
+			medium_sea_green = "2ECC71",
+			summer_sky = "3498DB",
+			deep_lilac = "9B59B6",
+			ruby = "E91E63",
+			moon_yellow = "F1C40F",
+			tahiti_gold = "E67E22",
+			cinnabar = "E74C3C",
+			submarine = "95A5A6",
+			blue_aquamarine = "607D8B",
+			deep_sea = "11806A",
+			sea_green = "1F8B4C",
+			endeavour = "206694",
+			vivid_violet = "71368A",
+			jazzberry_jam = "AD1457",
+			dark_goldenrod = "C27C0E",
+			rust = "A84300",
+			brown = "992D22",
+			gray_chateau = "979C9F",
+			bismark = "546E7A",
+			sti_blue = "0E4BEF",
+			wrx_blue = "00247D",
+			rallyart_crimson = "E60012",
+			lime = "00FF00",
+			forest_green = "228B22",
+			cadmium_green = "097969",
+			aquamarine = "7FFFD4",
+			blue_green = "088F8F",
+			raspberry = "E30B5C",
+			scarlet_red = "FF2400";
+	};
+};
+#endif
