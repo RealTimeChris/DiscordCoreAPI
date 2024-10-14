58bb3c3ae0dc1d752ce9ebf86b783c2f17419990
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2024-10-14 01:54:02 +0000

    [bot] VCPKG info update [skip ci]

--- Vcpkg/ports/discordcoreapi/portfile.cmake
+++ Vcpkg/ports/discordcoreapi/portfile.cmake
@@ -2,22 +2,16 @@ if(VCPKG_TARGET_IS_LINUX)
     message(WARNING "DiscordCoreAPI only supports g++ 11 on linux.")
 endif()
 
-# Set the number of parallel jobs to 1
-if(NOT DEFINED ENV{VCPKG_MAX_CONCURRENCY})
-    set(ENV{VCPKG_MAX_CONCURRENCY} 1)  # Set this to any desired limit (e.g., 4)
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO RealTimeChris/DiscordCoreAPI
     REF "v${VERSION}"
-    SHA512 2c162d2bcf67b8acb1fe124f6d6be15854a8d293ea1d9418b24cdeee25610fbd93a45c7203646f3ef6a58e5b69a36b9f3b63989c33484635c8db17f5b52b98af
+    SHA512 f7f0e2bf909b44e4212e8dab9c935d8f49099b89aa7033a7b2ca0a590b7dd08a1c1f1a7a5e347fb63a718a439cbfce58cc0ab1323f06c7f13888ee4504648076
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    DISABLE_PARALLEL_CONFIGURE
 )
 
 vcpkg_cmake_install()
